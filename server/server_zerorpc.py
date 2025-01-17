5e8aa662cef9b2039c7e51555f49ae8c3183feed
binny <binny@vip.163.com>
2025-01-17 15:30:40 +0800

    Debug mode and print debug message

--- server/server_zerorpc.py
+++ server/server_zerorpc.py
@@ -27,11 +27,14 @@ import os, sys
 
 import random
 import string
-para_path=os.path.dirname(os.path.dirname(__file__))
-para_path=os.path.realpath(os.path.join(para_path, 'wifite'))
-sys.path.append(para_path)
 
-from util.color_win import Color
+try:
+    from util.color_win import Color
+except (ValueError, ImportError) as e:
+    _para_path = os.path.dirname(os.path.dirname(__file__))
+    para_path = os.path.realpath(os.path.join(_para_path, 'wifite'))
+    sys.path.append(para_path)
+    from util.color_win import Color
 
 # pip install netifaces
 import netifaces as ni
@@ -41,25 +44,28 @@ try:
 except TypeError:
     TIME_FACTOR = 0.2
 
-def get_local_ip_using_netifaces(interface_name=None):
-    def get_address(interface):
-        interface_info = ni.ifaddresses(interface)
-        if ni.AF_INET in interface_info:
-            for link in interface_info[ni.AF_INET]:
-                ip_address = link['addr']
+def get_address_from_interface(interface, except_local=True):
+    interface_info = ni.ifaddresses(interface)
+    if ni.AF_INET in interface_info:
+        for link in interface_info[ni.AF_INET]:
+            ip_address = link['addr']
+            if except_local:
                 if ip_address != "127.0.0.1":
                     return ip_address
-        return None
-    
+            else:
+                return ip_address
+    return None
+
+def get_local_ip_using_netifaces(interface_name=None):
     interfaces = ni.interfaces()
     ip_addresses = []
     if interface_name and interface_name in interfaces:
-        ip_address = get_address(interface_name)
+        ip_address = get_address_from_interface(interface_name)
         if not ip_address is None:
             ip_addresses.append((ip_address, interface_name))
     else:
         for interface in interfaces:
-            ip_address = get_address(interface)
+            ip_address = get_address_from_interface(interface)
             if not ip_address is None:
                 ip_addresses.append((ip_address, interface))
     return ip_addresses
@@ -83,7 +89,7 @@ class MyServer(zerorpc.Server):
         ex_class_name = ex_class_name.replace("<class '", '')
         ex_class_name = ex_class_name.replace("'>", '')
         desc = f'Cmd={request}, Class={ex_class_name}, Msg={ex.args[0]}'
-        print('Error: exec_cmd_ret=>%s' % desc)
+        self.debug_error('print_error', desc)
         return desc
 
     def exec_cmd_ret(self, request):
@@ -117,23 +123,28 @@ class MyServer(zerorpc.Server):
 
     def debug_print(self, cmd, msg):
         if self.is_debug():
-            Color.pl("{Debug} {C}%s:{W} %s" % (cmd, msg))
+            Color.pl("{{R}Debug{W}} {C}%s:{W} %s" % (cmd, msg))
+
+    def debug_error(self, cmd, msg):
+        Color.pl("{{R}Error{W}} {C}%s:{W} %s" % (cmd, msg))
 
     def exec_cmd(self, request):
         self.debug_print("exec_cmd", request)
         try:
             exec(request, self.globals, self.locals)
         except Exception as e:
-            print('Exec: %s' % request)
-            print('Error: %s' % e)
+            self.debug_error('exec_cmd', request)
+            self.debug_error('e=', '%s' % e)
 
     def doCommand(self, request, debug=False):
-        print("doCommand in: %s" % request)
+        # print("doCommand in: %s" % request)
+        cmd = 'doCommand'
         if request == 'reset_namespace':
             self.init()
         if debug:
             self.globals['debug'] = debug
-        self.debug_print("doCommand", request)
+            cmd += '(Debug mode)'
+        self.debug_print(cmd, request)
 
     def get_file(self, request):
         self.debug_print("get_file", request)
@@ -174,7 +185,7 @@ class MyServer(zerorpc.Server):
         """
         self.debug_print("rename", "%s=>%s" % (source, destination))
         if not os.path.exists(source):
-            print(f"The file {source} not exist!")
+            self.debug_error('rename', f"The file {source} not exist!")
             return 'ErrorNotExist'
         else:
             try:
@@ -192,14 +203,14 @@ class MyServer(zerorpc.Server):
                 return f'ErrorUnknow:{e}'
 
 if __name__ == "__main__":
-    Color.pl("{W}Start server ..." )
+    Color.pl("{G}Start server ...{W}" )
     server_port = '12999'
     server_ip = '0.0.0.0'
     Color.pl('Listen to {C}%s{W}' % f'{server_ip}:{server_port}')
     ip_addrs = get_local_ip_using_netifaces()
     for ip_addr, interface in ip_addrs:
         ipinfo = f'{ip_addr}:{server_port}'
-        Color.pl('Client connect to {C}%s{W} (Interface={C}%s{W}' % (ipinfo, interface))
+        Color.pl('Client connect to {C}%s{W} ( Interface={O}%s{W} )' % (ipinfo, interface))
     connet_str = f"tcp://{server_ip}:{server_port}"
     server = MyServer()
     server.bind(connet_str)
